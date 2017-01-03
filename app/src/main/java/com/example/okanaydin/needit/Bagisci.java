e7fc4b5998d9aed62cb7043be0ae38b53a487287
okanaydin <okanaydin1994@gmail.com>
2017-01-04 00:15:46 +0200

    Bagisci Activity

--- app/src/main/java/com/example/okanaydin/needit/Bagisci.java
+++ app/src/main/java/com/example/okanaydin/needit/Bagisci.java
@@ -9,5 +9,7 @@ public class Bagisci extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bagisci);
+
+
     }
 }
